fc775cae4fc893de8bdaba484032fdda541a1c57
Silmiuww <nadiasalma078@gmail.com>
2024-12-13 07:29:50 +0700

    progres

--- DaftarPesanan.java
+++ DaftarPesanan.java
@@ -1,16 +1,16 @@
+
 public static void tampilkanPesanan() {
-    if (daftarPesanan.isEmpty()) {
+    if (jmlPesanan == 0) {
         System.out.println("Belum ada pesanan.");
         return;
     }
 
     System.out.println("\n===== DAFTAR PESANAN =====");
-    for (Pesanan pesanan : daftarPesanan) {
-        System.out.println("Nama Pelanggan: " + pesanan.namaPelanggan);
-        System.out.println("Nomor Meja: " + pesanan.nomorMeja);
+    for (int i = 0; i < jmlPesanan; i++) {
+        System.out.println("Nama Pelanggan: " + pesanan[i][0]);
+        System.out.println("Nomor Meja: " + pesanan[i][1]);
         System.out.println("Detail Pesanan:");
-        System.out.printf("- %s x %d = Rp %d\n", pesanan.namaMenu, pesanan.jumlahItem, pesanan.totalHarga);
-        System.out.println("Total Harga Pesanan: Rp " + pesanan.totalHarga);
+        System.out.printf("- %s x %s = Rp %s\n", pesanan[i][2], pesanan[i][3], pesanan[i][4]);
         System.out.println("-----------------------------");
     }
 }
