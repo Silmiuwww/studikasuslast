8930186167eb590a3eb1fabc5f36af8ebaa8c10c
Silmiuww <nadiasalma078@gmail.com>
2024-12-14 11:05:36 +0700

    progres

--- DaftarPesanan.java
+++ DaftarPesanan.java
@@ -1,16 +1,30 @@
+import java. util. Scanner;
 
 public static void tampilkanPesanan() {
+    System.out.println("\n===== DAFTAR PESANAN =====\n");
+
     if (jmlPesanan == 0) {
-        System.out.println("Belum ada pesanan.");
+        System.out.println("Belum ada pesanan yang tercatat.");
         return;
     }
 
-    System.out.println("\n===== DAFTAR PESANAN =====");
     for (int i = 0; i < jmlPesanan; i++) {
         System.out.println("Nama Pelanggan: " + pesanan[i][0]);
         System.out.println("Nomor Meja: " + pesanan[i][1]);
         System.out.println("Detail Pesanan:");
-        System.out.printf("- %s x %s = Rp %s\n", pesanan[i][2], pesanan[i][3], pesanan[i][4]);
-        System.out.println("-----------------------------");
+
+        int totalHargaPesanan = 0;
+        for (int j = 0; j < jmlPesanan; j++) {
+            if (pesanan[j][0].equals(pesanan[i][0]) && pesanan[j][1].equals(pesanan[i][1])) {
+                int jumlahItem = Integer.parseInt(pesanan[j][3]);
+                int hargaTotal = Integer.parseInt(pesanan[j][4]);
+                totalHargaPesanan += hargaTotal;
+
+                System.out.println("- " + pesanan[j][2] + " x " + jumlahItem + " = Rp " + hargaTotal);
+            }
+        }
+
+        System.out.println("Total Harga Pesanan: Rp " + totalHargaPesanan);
+        System.out.println("--------------------------------------");
     }
 }
